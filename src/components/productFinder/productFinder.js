c34848ce88cc0e8a19e1b47898bca560b9ba7955
Paul WB <higgsbison@gmail.com>
2017-02-04 16:43:52 -0800

    add search filter to product finder

--- src/components/productFinder/productFinder.js
+++ src/components/productFinder/productFinder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 import SearchBar from 'react-native-searchbar';
 import Button from 'apsl-react-native-button';
@@ -12,20 +13,50 @@ import { toggleFilter, selectProduct, closeProduct } from '../../actions';
 
 import { categories } from '../../data';
 
-class Products extends Component {
+class ProductFinder extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state= {
+      searchTerm: '',
+      currentProducts: this.props.data,
+    }
+    this.openModal = this.openModal.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      currentProducts:nextProps.data,
+    });
+  }
+  handleSearch(text) {
+    const reg = new RegExp(`${text}`,'gi')
+    const filteredProducts = _.filter(this.props.data, product => {
+      return reg.test(product.name);
+    });
+    this.setState({
+      searchTerm: text,
+      currentProducts: filteredProducts,
+    });
+  }
   openModal(product) {
     return () => { this.props.selectProduct(product); };
   }
   render() {
-    const { selected, filter, data } = this.props;
+    const { selected, filter } = this.props;
     return (
       <View style={{ flex: 1, top: -20 }}>
         {selected && <OrderModal product={selected} closeModal={this.props.closeProduct} />}
-        <SearchBar showOnLoad hideBack heightAdjust={-10} />
+        <SearchBar
+          showOnLoad
+          hideBack
+          heightAdjust={-10}
+          handleChangeText={text => { this.handleSearch(text) }}
+        />
         <View style={{ flex: 1, flexDirection: 'column', marginTop: 80 }}>
           <Button onPress={this.props.toggleFilter}> Filter </Button>
           {filter && <GridList categories={categories} />}
-          <ProductList data={data} onPress={this.openModal.bind(this)} />
+          <ProductList data={this.state.currentProducts} onPress={this.openModal} />
         </View>
       </View>
     );
@@ -36,4 +67,6 @@ const mapStateToProps = ({ products }) => {
   return { filter, selected };
 };
 
-export default connect(mapStateToProps, { toggleFilter, selectProduct, closeProduct })(Products);
+export default connect(mapStateToProps, {
+  toggleFilter, selectProduct, closeProduct
+})(ProductFinder);
